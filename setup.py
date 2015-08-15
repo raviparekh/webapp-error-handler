08ffda22ff8f7bd40471cccca09c351bdabb44f6
Ravi Parekh <admin@rparekh.com>
2015-08-15 19:41:44 +0100

    Added setup.py and manifest

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='flask_error_handler',
+      version='1.0.0',
+      description='An Alternative flask error handling',
+      url='https://github.com/raviparekh/flask-error-handler',
+      author='Ravi Parekh',
+      license='MIT',
+      classifiers=['License ::MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development :: Utilities'],
+      keywords='error-handling flask-error-handler',
+      install_requires=['flask==0.10.1'])
+
+
+
