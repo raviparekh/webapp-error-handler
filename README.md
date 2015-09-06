779f456f87670c7c52cba6ae27d6851755068a00
Ravi Parekh <admin@rparekh.com>
2015-09-07 00:01:51 +0100

    Updated read me and setup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![PyPI version](https://badge.fury.io/py/flask_error_handler.svg)](http://badge.fury.io/py/flask_error_handler)
 [![Build Status](https://travis-ci.org/raviparekh/flask-error-handler.svg?branch=master)](https://travis-ci.org/raviparekh/flask-error-handler)
 [![Coverage Status](https://coveralls.io/repos/raviparekh/flask-error-handler/badge.svg?branch=master&service=github)](https://coveralls.io/github/raviparekh/flask-error-handler?branch=master)
 
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(name='flask_error_handler',
-      version='1.0.0',
+      version='1.1.0',
       description='An Alternative flask error handling',
       url='https://github.com/raviparekh/flask-error-handler',
       author='Ravi Parekh',
       license='MIT',
       classifiers=['License :: OSI Approved :: MIT License',
                    'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
                    'Intended Audience :: Developers',
                    'Topic :: Software Development :: Libraries'],
       keywords='error-handling flask-error-handler',
